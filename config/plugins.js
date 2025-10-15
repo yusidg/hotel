2763510fb7fdbdc9a01c5335eb16010cae42ce13
AmineshOptisoft <aminesh.optisoft.tester@gmail.com>
2025-10-15 19:32:09 +0530

    NEW-CHANGES

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,33 @@
 module.exports = ({ env }) => ({
-    email: {
-      config: {
-        provider: 'nodemailer',
-        providerOptions: {
-          host: env('SMTP_HOST', 'smtp.gmail.com'),
-          port: env('SMTP_PORT', 587),
-          auth: {
-            user: env('SMTP_USERNAME'),
-            pass: env('SMTP_PASSWORD'),
-          },
-        },
-        settings: {
-          defaultFrom: 'habibkd391@gmail.com',
-          defaultReplyTo: 'yusidg709@gmail.com',
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.gmail.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
         },
       },
+      settings: {
+        defaultFrom: 'habibkd391@gmail.com',
+        defaultReplyTo: 'yusidg709@gmail.com',
+      },
     },
-  });
-  
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
